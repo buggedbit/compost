98fb6f952d7e33fbdb90807790fa099283962ee1
Yashasvi-Sriram <yash.3997@gmail.com>
2016-11-16 00:51:43 +0530

    fixes : bug in index.php         prev : allows < > ' " & in book and chapter names         now : checks that in them also
    adds : count feature for books and chapters
    
    improves : css

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <!--LIB HEADERS-->
-    <link href="https://fonts.googleapis.com/css?family=Pacifico|VT323" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Audiowide|Crafty+Girls" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -12,13 +12,24 @@
     <link rel="icon" href="../static/logo.png">
     <!--CSS INCLUDES-->
     <style>
-        body {
-            background-image: url("https://s-media-cache-ak0.pinimg.com/originals/8c/9c/4f/8c9c4f9d9305496ae5164b271ca4be03.jpg");
-        	font-family: 'VT323', monospace;
+        ::-moz-selection { /* Code for Firefox */
+            color: white;
+            background: darkorange;
         }
 
-        .modal{
-        	font-size: xx-large;
+        ::selection {
+            color: white;
+            background: darkorange;
+        }
+
+        body {
+            background-image: url("http://www.walldevil.com/wallpapers/a82/wallpaper-graphic-design-colorful-square-background-texture-art-desktop-minimalist-black.jpg");
+            font-family: 'Crafty Girls', cursive;
+            font-size: xx-large;
+        }
+
+        .modal {
+            font-size: xx-large;
         }
 
         .writable {
@@ -28,41 +39,32 @@
             overflow: auto;
             padding: 10px;
             cursor: pointer;
-            color: black;
-            background-color: rgba(255, 255, 255, 0.40);
-            font-family: 'VT323', monospace;
+            background-color: rgba(0, 0, 0, 0.60);
+            color: white;
         }
 
         .writable:focus {
             outline-style: dashed;
             outline-color: white;
-            outline-offset: 2px;
+            outline-width: thin;
+            outline-offset: 1px;
         }
 
         .list {
             font-size: large;
             max-height: 100%;
-            background-color: rgba(255, 255, 255, 0.40);
-            font-family: 'Pacifico', cursive;
-        }
-
-        .list tr:hover {
+            font-family: 'Audiowide', cursive;
         }
 
         .select:hover {
             cursor: pointer;
         }
 
-        .list a {
-            color: black;
-            text-decoration: none;
-        }
-
         .search {
             font-size: large;
-            color: black;
             text-align: center;
-            background-color: rgba(255, 255, 255, 0.90);
+            color: white;
+            background-color: transparent;
         }
 
         .floating_button {
@@ -74,16 +76,17 @@
             right: 20px;
             z-index: 9990;
             font-family: monospace;
-            background-color: deeppink;
+            font-size: x-large;
+            background-color: white;
             color: white;
             opacity: 0.95;
-            border: dotted;
+            border-width: 0;
         }
 
         .floating_button:hover {
             background-color: deeppink;
             color: white;
-            font-size: x-large;
+            font-size: xx-large;
             opacity: 1.0;
         }
 
@@ -96,20 +99,19 @@
         .message_box {
             height: 500px;
             width: 500px;
-            border-radius: 250px;
             position: fixed;
-            top: 20%;
-            left: 100%;
-            background-color: rgba(0, 23, 255, 0.78);
+            bottom: 0;
+            left: 0;
+            background-color: rgba(91, 91, 91, 0.95);
             z-index: 9999;
+            padding: 20px;
             text-align: center;
         }
 
         .message {
             color: white;
             position: relative;
-            top: 200px;
-            font-size: 50px;
+            font-family: 'Audiowide', cursive;
         }
 
     </style>
@@ -130,12 +132,17 @@
             /* read part */
             this.populate_book_list = function () {
                 var list = this.context.book_list();
-                if (list == undefined)return;
+                var book_count = $('#book_count');
+                if (list == undefined) {
+                    $(book_count).html("0");
+                    return;
+                }
+                $(book_count).html(String(list.length));
                 var book_list = $('#book_list');
                 var html1 = "<tr><td class='select' id='";
                 var html2 = "'><a href='#'>";
                 var html3 = "</a></td><td><button id='";
-                var html4 = "'class='btn-danger delete'>X</button></td><td><button id='";
+                var html4 = "'class='btn-danger delete'>del</button></td><td><button id='";
                 var html5 =
                     "'class='btn-success create' " +
                     "data-toggle='modal' " +
@@ -163,12 +170,17 @@
 
             this.populate_chapter_list = function (book_pk) {
                 var list = this.context.chapter_list(book_pk);
-                if (list == undefined)return;
+                var chapter_count = $('#chapter_count');
+                if (list == undefined) {
+                    $(chapter_count).html("0");
+                    return;
+                }
+                $(chapter_count).html(String(list.length));
                 var chapter_list = $('#chapter_list');
                 var html1 = "<tr><td class='select' id='";
                 var html2 = "'><a href='#'>";
                 var html3 = "</a></td><td><button id='";
-                var html4 = "'class='btn-danger delete'>X</button></td></tr>";
+                var html4 = "'class='btn-danger delete'>del</button></td></tr>";
 
                 $(chapter_list).empty();
                 for (var i = 0; i < list.length; ++i) {
@@ -195,6 +207,9 @@
                     var id = $(this).attr('id');
                     context.book_delete(Number(id.split("_")[4]));
                     $(this).parent().parent().remove();
+                    var book_count_hint = $('#book_count');
+                    var book_count = Number($(book_count_hint).html());
+                    $(book_count_hint).html(String(book_count - 1));
                 }).on('click', '.create', function () {
                     var id = $(this).attr('id');
                     var first_chapter = context.first_chapter(Number(id.split("_")[5]));
@@ -209,6 +224,9 @@
                     var id = $(this).attr('id');
                     context.chapter_delete(context.book_pk, Number(id.split("_")[4]));
                     $(this).parent().parent().remove();
+                    var chapter_count_hint = $('#chapter_count');
+                    var chapter_count = Number($(chapter_count_hint).html());
+                    $(chapter_count_hint).html(String(chapter_count - 1));
                 });
 
                 $('#book_search').on('keyup', function () {
@@ -216,15 +234,18 @@
                     var regex = new RegExp(pattern, 'i');
                     var book_list = context.book_list();
                     if (book_list == undefined)return;
+                    var count = 0;
                     for (var i = 0; i < book_list.length; ++i) {
                         var name = book_list.item(i).getAttribute('name');
                         if (regex.test(name)) {
                             $('#book_list_item_' + book_list.item(i).getAttribute('pk')).parent().fadeIn(0);
+                            count++;
                         }
                         else {
                             $('#book_list_item_' + book_list.item(i).getAttribute('pk')).parent().fadeOut(0);
                         }
                     }
+                    $('#book_count').html(String(count));
                 });
 
                 $('#chapter_search').on('keyup', function () {
@@ -232,15 +253,18 @@
                     var regex = new RegExp(pattern, 'i');
                     var chapter_list = context.chapter_list(context.book_pk);
                     if (chapter_list == undefined)return;
+                    var count = 0;
                     for (var i = 0; i < chapter_list.length; ++i) {
                         var name = chapter_list.item(i).getAttribute('name');
                         if (regex.test(name)) {
                             $('#chapter_list_item_' + chapter_list.item(i).getAttribute('pk')).parent().fadeIn(0);
+                            count++;
                         }
                         else {
                             $('#chapter_list_item_' + chapter_list.item(i).getAttribute('pk')).parent().fadeOut(0);
                         }
                     }
+                    $('#chapter_count').html(String(count));
                 });
 
             };
@@ -249,12 +273,12 @@
                 /* assuming that context.book_pk and context.chapter_pk
                  point to the currently viewing book and chapter */
                 $('#book_name').on('keyup', function () {
-                	var regex = new RegExp("[<>&\"']");
-                	var illegal = regex.test($(this).val());
-                	if (illegal){
-                		alert("< , > , & , \" , ' are not allowed");
-                		return;
-                	}
+                    var regex = new RegExp("[<>&\"']");
+                    var illegal = regex.test($(this).val());
+                    if (illegal) {
+                        alert("< , > , & , \" , ' are not allowed");
+                        return;
+                    }
                     var book = context.book(context.book_pk);
                     if (book == undefined)return;
                     var book_list_item = $('#book_list_item_' + book.getAttribute('pk'));
@@ -263,11 +287,11 @@
                 });
                 $('#chapter_name').on('keyup', function () {
                     var regex = new RegExp("[<>&\"']");
-                	var illegal = regex.test($(this).val());
-                	if (illegal){
-                		alert("< , > , & , \" , ' are not allowed");
-                		return;
-                	}
+                    var illegal = regex.test($(this).val());
+                    if (illegal) {
+                        alert("< , > , & , \" , ' are not allowed");
+                        return;
+                    }
                     var chapter = context.chapter(context.book_pk, context.chapter_pk);
                     if (chapter == undefined)return;
                     var chapter_list_item = $('#chapter_list_item_' + chapter.getAttribute('pk'));
@@ -276,11 +300,11 @@
                 });
                 $('#chapter_content').on('keyup', function () {
                     var regex = new RegExp("[<>&\"']");
-                	var illegal = regex.test($(this).val());
-                	if (illegal){
-                		alert("< , > , & , \" , ' are not allowed");
-                		return;
-                	}
+                    var illegal = regex.test($(this).val());
+                    if (illegal) {
+                        alert("< , > , & , \" , ' are not allowed");
+                        return;
+                    }
                     var chapter = context.chapter(context.book_pk, context.chapter_pk);
                     if (chapter == undefined)return;
                     chapter.textContent = $(this).val();
@@ -300,20 +324,32 @@
             this.initCreateListeners = function (context) {
                 $('#chapter_create').on('click', function () {
                     var name = $('#chapter_create_name').val();
+                    var regex = new RegExp("[<>&\"']");
+                    var illegal = regex.test(name);
+                    if (illegal) {
+                        alert("< , > , & , \" , ' are not allowed");
+                        return;
+                    }
                     if (context.chapter_create(name, context.book_pk)) {
                         manager.populate_chapter_list(context.book_pk);
                         manager.showMessage("New Chapter "
                             + name
-                            + "created in "
+                            + " created in "
                             + context.book(context.book_pk).getAttribute('name'));
                     }
                 });
 
                 $('#book_create').on('click', function () {
                     var name = $('#book_create_name').val();
+                    var regex = new RegExp("[<>&\"']");
+                    var illegal = regex.test(name);
+                    if (illegal) {
+                        alert("< , > , & , \" , ' are not allowed");
+                        return;
+                    }
                     if (context.book_create(name)) {
                         manager.populate_book_list();
-                        manager.showMessage("New Book "
+                        manager.showMessage(" New Book "
                             + name);
                     }
                 });
@@ -377,16 +413,7 @@
             this.showMessage = function (message) {
                 $('#message').text(message);
                 var message_box = $('#message_box');
-                $(message_box)
-                    .animate(
-                        {
-                            left: $(message_box).parent().width() / 2 - 250
-                        }, 'fast')
-                    .delay(3000)
-                    .animate(
-                        {
-                            left: '100%'
-                        }, 'fast');
+                $(message_box).slideDown().delay(1000).slideUp();
             };
         }
 
@@ -397,14 +424,14 @@
             /* In built's */
             $('[data-toggle="tooltip"]').tooltip();
             if (!String.prototype.encodeHTML) {
-			  String.prototype.encodeHTML = function () {
-			    return this.replace(/&/g, '&amp;')
-			               .replace(/</g, '&lt;')
-			               .replace(/>/g, '&gt;')
-			               .replace(/"/g, '&quot;')
-			               .replace(/'/g, '&apos;');
-			  };
-			}
+                String.prototype.encodeHTML = function () {
+                    return this.replace(/&/g, '&amp;')
+                        .replace(/</g, '&lt;')
+                        .replace(/>/g, '&gt;')
+                        .replace(/"/g, '&quot;')
+                        .replace(/'/g, '&apos;');
+                };
+            }
             $(window).bind('beforeunload', function () {
 //                manager.synchronize();
             });
@@ -440,7 +467,8 @@
                placeholder="Find Chapters">
         <br>
         <br>
-        <table class="table table-hover table-striped">
+        <table class="table table-hover">
+            <h3 style="color: white;"><span id="chapter_count"></span> chapters available </h3>
             <tbody id="chapter_list"
                    class="list">
 
@@ -448,34 +476,33 @@
         </table>
     </div>
     <div class="col-xs-8">
-        <div class="row">
-            <div class="col-xs-8">
-                <blockquote>
+        <blockquote>
+            <div class="row">
+                <div class="col-xs-8">
                     <input id="chapter_name"
                            class="writable text-info"
-                           style="font-size: x-large;"
+                           style="font-size: x-large;background-color: transparent"
                            data-toggle="tooltip"
                            data-placement="left"
                            title="Chapter"
                            type="text"
                            placeholder="Chapter">
-                </blockquote>
+                </div>
+                <div class="col-xs-1"></div>
+                <div class="col-xs-3">
+                    <input id="book_name"
+                           class="writable"
+                           style="text-align: right;
+                              font-size: larger;
+                              background-color: transparent"
+                           data-toggle="tooltip"
+                           data-placement="right"
+                           title="Book"
+                           type="text"
+                           placeholder="Book">
+                </div>
             </div>
-            <div class="col-xs-1"></div>
-            <div class="col-xs-3">
-                <blockquote class="blockquote-reverse">
-                <input id="book_name"
-                       class="writable"
-                       style="text-align: right;
-                                  font-size: larger;"
-                       data-toggle="tooltip"
-                       data-placement="right"
-                       title="Book"
-                       type="text"
-                       placeholder="Book">
-                </blockquote>
-            </div>
-        </div>
+        </blockquote>
         <textarea id="chapter_content"
                   class="writable"
                   style="font-size: x-large;max-height: 800px;height: 800px"
@@ -491,7 +518,8 @@
                placeholder="Find Books">
         <br>
         <br>
-        <table class="table table-hover table-striped">
+        <table class="table table-hover ">
+            <h3 style="color: white;"><span id="book_count"></span> books available </h3>
             <tbody id="book_list"
                    class="list">
             </tbody>
@@ -499,17 +527,22 @@
     </div>
 </div>
 
+<!--SYNCHRONIZE-->
 <button id="synchronize"
-        class="floating_button btn btn-default"
+        class="floating_button btn-default"
+        style="background-color: white;color: black;"
         data-toggle="tooltip"
         title="Synchronize">
     <span class="glyphicon glyphicon-refresh"></span>
 </button>
+
 <!--MESSAGE BOX-->
 <div id="message_box"
-     class="message_box">
-    <span id="message" class="message"></span>
+     class="message_box"
+     style="display: none">
+    <p id="message" class="message"></p>
 </div>
+
 <!--NEW CHAPTER-->
 <div class="modal fade"
      id="chapter_create_modal"
@@ -563,7 +596,7 @@
 </div>
 
 <button class="floating_button"
-        style="background-color: darkgreen;right: 160px;"
+        style="background-color: white;color: black;right: 160px;"
         data-toggle="modal"
         title="New Book"
         data-target="#book_create_modal">
@@ -572,8 +605,8 @@
           data-placement="top"
           class="glyphicon glyphicon-book"></span>
 </button>
-<!--LOG-->
 
+<!--LOG-->
 <div class="modal fade"
      id="log_modal"
      role="dialog">
@@ -600,14 +633,14 @@
 </div>
 
 <button class="floating_button"
-        style="background-color: darkblue;bottom: 160px;"
+        style="background-color: white;color: black;bottom: 160px;"
         data-toggle="modal"
         title="New Book"
         data-target="#log_modal">
     <span data-toggle="tooltip"
           title="Log"
           data-placement="top"
-          class="glyphicon glyphicon-log-in"></span>
+          class="glyphicon glyphicon-off"></span>
 </button>
 
 </body>
