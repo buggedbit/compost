631f68324475e0a1cde21e3d9fe151297c8a9131
Yashasvi-Sriram <yash.3997@gmail.com>
2017-02-19 00:08:38 +0530

    initializes tictoc

---
+++ TicToc/Constants.java
@@ -0,0 +1,44 @@
+package com.yashasvi.calender;
+
+/**
+ * Contains constants related to<br>
+ * Time conversions,<br>
+ * Date, Time, DateTime Classes<br>
+ * Gregorian Calender<br>
+ *
+ * @author pandu
+ * @version 1.0
+ */
+public class Constants {
+
+    public static final String SPACE_SEP = " ";
+    public static final String TAG = "Calender says: ";
+
+
+    public static final int MINUTES_IN_HOUR = 60;
+    public static final int MINUTES_IN_DAY = 1440;
+    public static final int SECONDS_IN_MINUTE = 60;
+    public static final int SECONDS_IN_HOUR = 3600;
+    public static final int SECONDS_IN_DAY = 86400;
+    public static final int HOURS_IN_DAY = 24;
+
+    public static final int DAYS_IN_WEEK = 7;
+    public static final int DAYS_IN_STD_MONTH = 30;
+    public static final int DAYS_IN_STD_YEAR = 365;
+    public static final int[] STD_MONTH_EXTRAS_ARRAY = {1, -1, 0, 0, 1, 1, 2, 3, 3, 4, 4, 5};
+
+    public static final String[] MONTH_NAMES = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    public static final String[] SUFFIXES = {"st", "nd", "rd", "th"};
+    public static final int[] DAYS_IN_MONTH_NLY = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static final int[] DAYS_IN_MONTH_LY = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public static Time START_OF_THE_DAY = new Time("00 00 00", " ");
+}
+
+
+
+
+
+
+
+
---
+++ TicToc/DateTimeDelta.java
@@ -0,0 +1,113 @@
+package com.yashasvi.calender;
+
+/* For getting log uncomment the comments starting with (android) */
+//(android) import android.util.Log;
+
+import static java.lang.Integer.parseInt;
+
+
+// DateTimeDelta class is explicitly defined for maintaining difference in dateTime
+// So use develop it accordingly
+public class DateTimeDelta {
+    // as there is no such thing as a VALID DateTimeDelta
+    // restrict with only a
+    // public DateTimeDelta(long $24hr,int $sec) constructor
+    // i.e as there is no default constructor,
+    // the DateTimeDelta is always VALID and SET whenever it is initialized
+
+    public long $24hr = 0L;
+    public long $sec = 0L;
+
+    public DateTimeDelta(long $24hr, int $sec) {
+        this.$24hr = $24hr;
+        this.$sec = $sec;
+    }
+
+    public DateTimeDelta(long $24hr, long $sec) {
+        this.$24hr = $24hr;
+        this.$sec = $sec;
+    }
+
+    //String format = x<space>hrs<space>y<space>min
+    public DateTimeDelta(String dateTimeDiffString) {
+        String[] comp = dateTimeDiffString.split(Constants.SPACE_SEP);
+        try {
+            int hr = parseInt(comp[0]);
+            int min = parseInt(comp[2]);
+
+            this.$24hr = 0;
+            this.$sec = hr * Constants.SECONDS_IN_HOUR + min * Constants.SECONDS_IN_MINUTE;
+        } catch (Exception e) {
+            //(android) Log.i(Constants.TAG, "DateTimeDelta: not a proper DateTimeDelta object initialization with " + dateTimeDiffString);
+        }
+    }
+
+    //String Format = Days<sep>Seconds<sep>
+    //String Format = Days<sep>Seconds
+    public DateTimeDelta(String dateTimeDiffString, String sep) {
+
+        String error = "DateTimeDelta: not a proper DateTimeDelta object initialization with "
+                + "\n string " + dateTimeDiffString
+                + "\n and with separator " + sep;
+
+        if (dateTimeDiffString != null && !dateTimeDiffString.matches("")
+                && sep != null && !sep.matches("")
+                ) {
+            String[] day_sec = dateTimeDiffString.split(sep);
+            if (day_sec.length == 2) {
+                try {
+                    this.$24hr = Long.parseLong(day_sec[0]);
+                    this.$sec = Long.parseLong(day_sec[1]);
+                } catch (Exception e) {
+                    //(android) Log.i(Constants.TAG, error);
+                }
+            } else {
+                //(android) Log.i(Constants.TAG, error);
+            }
+        } else {
+            //(android) Log.i(Constants.TAG, error);
+        }
+    }
+
+    public DateTimeDelta(long minutes) {
+        this.$24hr = minutes / Constants.MINUTES_IN_DAY;
+        this.$sec = (minutes % Constants.MINUTES_IN_DAY) * Constants.SECONDS_IN_MINUTE;
+    }
+
+    public long weeksDiff() {
+        return this.$24hr / 7;
+    }
+
+    public long secondsDiff() {
+        return this.$24hr * Constants.SECONDS_IN_DAY + this.$sec;
+    }
+
+    public long minutesDiff() {
+        return this.$24hr * Constants.MINUTES_IN_DAY + this.$sec / Constants.SECONDS_IN_MINUTE;
+    }
+
+    public String daySecRepresentation() {
+        return this.$24hr + Constants.SPACE_SEP + "day"
+                + Constants.SPACE_SEP + this.$sec + Constants.SPACE_SEP + "second";
+    }
+
+    public String hourMinRepresentation() {
+        long _hr = (int) (this.$24hr * Constants.HOURS_IN_DAY);
+        _hr = _hr + this.$sec / Constants.SECONDS_IN_HOUR;
+        int _min = (int) (this.$sec % Constants.SECONDS_IN_HOUR) / Constants.SECONDS_IN_MINUTE;
+
+        return _hr + Constants.SPACE_SEP + "hr" + Constants.SPACE_SEP
+                + _min + Constants.SPACE_SEP + "min";
+    }
+
+    public static boolean isValidHrMinRepresentation(String dateTimeDiffString) {
+        String[] comp = dateTimeDiffString.split(Constants.SPACE_SEP);
+        try {
+            Integer.parseInt(comp[0]);
+            Integer.parseInt(comp[2]);
+        } catch (Exception e) {
+            //(android) Log.i(Constants.TAG, "DateTimeDelta: not a proper DateTimeDelta object initialization with " + dateTimeDiffString);
+        }
+        return false;
+    }
+}
