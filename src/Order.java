e45c73d307458fc47a4c0a22ba4aba19653cc424
Yashasvi-Sriram <yash.3997@gmail.com>
2017-06-06 16:59:14 +0530

    creates Order class         1.fields delivery_address, part_clone_count_map         2.implements constructor with Address param         3.implements method addPart(String part, int clone_count)

---
+++ src/Order.java
@@ -0,0 +1,40 @@
+import java.util.Map;
+
+/**
+ * Order
+ */
+public class Order {
+    /**
+     * Delivery address
+     */
+    Address delivery_address;
+    /**
+     * Ordered parts and their clone counts
+     * map : part id -> clone count
+     */
+    Map<String, Integer> part_clone_count_map;
+
+    public Order(Address delivery_address) {
+        this.delivery_address = delivery_address;
+    }
+
+    /**
+     * Adds the part to the order
+     * If the part already exists in the order, the clone count is sum of previous and present clone counts
+     * Else adds the part to the order with the given clone count
+     */
+    public void addPart(String part, int clone_count) {
+
+        // Part exists in the list
+        if (this.part_clone_count_map.containsKey(part)) {
+            int prev_clone_count = this.part_clone_count_map.get(part);
+            this.part_clone_count_map.put(part, prev_clone_count + clone_count);
+        }
+        // Part is not in list
+        else {
+            this.part_clone_count_map.put(part, clone_count);
+        }
+
+    }
+
+}
