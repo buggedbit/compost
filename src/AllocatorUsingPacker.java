b0187a512a37eb427bd4d8290a41147e4f1cf377
Rishabh Shah <rishrocks17@gmail.com>
2017-06-17 00:24:45 -0600

    EasyPostAPI implementation, Progress in Allocator Using Packer.

--- src/AllocatorUsingPacker.java
+++ src/AllocatorUsingPacker.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+
 
 import MapsAPI.Response;
 
@@ -14,12 +18,38 @@ public class AllocatorUsingPacker {
 	    // Sort all responses w.r.t distance, in turn sorting the warehouses
 	    Collections.sort(allResponses, (Response r1, Response r2) -> r1.distanceCompare(r2));
 	    
-	    // Sorting warehouses w.r.t distance
+	    // Sorting warehouses w.r.t distance 
 	    ArrayList<Warehouse> sortedWarehouses = new ArrayList<>();
 	    for (Response r : allResponses)
 	    	sortedWarehouses.add(responseWarehouseMap.get(r));
 
+	    //TODO Get Order partList. 
+	    ArrayList<Part> sortedPartList = new ArrayList<>();
 	    
-	
+	    //Store availability of parts and find the rare items. 
+	    Map<Integer, Part> priorityMap = new HashMap<>();
+	    for (Part part : sortedPartList) {
+			
+	    	part.availability = new HashSet<>();
+			
+	    	for (Integer i = 0; i < sortedWarehouses.size(); i++) {
+				if(sortedWarehouses.get(i).inventory.containsKey(part.id))
+					part.availability.add(i);
+			}
+	    	if(part.availability.size() == 1){
+	    		Integer whIndex = part.availability.iterator().next();
+	    		priorityMap.put(whIndex, part);
+	    	}
+	    }
+	    
+	    //*Sort the Priority Map in descending order of distance to customer.
+	    //(So small items can fill without an extra box)
+	    ArrayList<Integer> sortedPriorityWarehouses= new ArrayList<>();
+	    for (Integer i : priorityMap.keySet())
+			sortedPriorityWarehouses.add(i);
+	    Collections.sort(sortedPriorityWarehouses, Collections.reverseOrder());
+	    
+	    //Sort order w.r.t availability and volume.
+	    Collections.sort(sortedPartList, (Part p1, Part p2)-> p1.preferentialCompare(p2));
 	}
 }
---
+++ src/MapsAPI/EasyPostAPI.java
@@ -0,0 +1,55 @@
+package MapsAPI;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.easypost.EasyPost;
+import com.easypost.exception.EasyPostException;
+import com.easypost.model.Address;
+import com.easypost.model.Parcel;
+import com.easypost.model.Shipment;
+
+public class EasyPostAPI {
+	
+	public static Address getAddress(String name, String street, String city, String state, String zip) throws EasyPostException{
+		Map<String, Object> addressMap = new HashMap<String, Object>();
+		addressMap.put("name", name);
+		addressMap.put("street1", street);
+        addressMap.put("city", city);
+        addressMap.put("state", state);
+        addressMap.put("zip", zip);
+        addressMap.put("country", "Canada");
+        return Address.create(addressMap);
+	}
+	public static Parcel getParcel(Double weight, Double height, Double length, Double width) throws EasyPostException{
+		Map<String, Object> parcelMap = new HashMap<String, Object>();
+        parcelMap.put("weight", 22.9);
+        parcelMap.put("height", 12.1);
+        parcelMap.put("width", 8);
+        parcelMap.put("length", 19.8);
+        return Parcel.create(parcelMap);
+	}
+	public static Shipment getShipment(Address toAddress, Address fromAddress, Parcel parcel) throws EasyPostException{
+		Map<String, Object> shipmentMap = new HashMap<String, Object>();
+		shipmentMap.put("to_address", toAddress);
+        shipmentMap.put("from_address", fromAddress);
+        shipmentMap.put("parcel", parcel);
+        return Shipment.create(shipmentMap);
+	}
+    public static void main(String[] args) throws EasyPostException {
+     	EasyPost.apiKey = "MBeHR0wTmyvsqxabxbCNFA"; //PartsAvatar
+//    	EasyPost.apiKey = "O6JFoCWqQHBR32We6FfOnw"; //Rishabh.Test
+//    	EasyPost.apiKey = "k51nSOHibTPFc82J8fh6eQ";	//Rishabh.Production
+
+     	Address warehouseAddress = getAddress("McKesson", "71 Glacier St", "Coquitlam","BC","V3K5Z1");
+        Address deliveryAddress = getAddress("RECEIVER", "11754 170 St NW","Edmonton", "AB", "T5S1J7");
+        Parcel parcel = getParcel(22.9, 12.1, 19.8, 8.0);
+        
+       
+//        Address verified = warehouseAddress.verify();
+//        System.out.println(verified.prettyPrint());
+        
+        Shipment shipment = getShipment(warehouseAddress, deliveryAddress, parcel);
+        System.out.println(shipment.prettyPrint());
+    }
+}
