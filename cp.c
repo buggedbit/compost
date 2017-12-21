b1b2dcaef39c2555b8b5a217dd2c2d7069d14eea
Yashasvi-Sriram <yash.3997@gmail.com>
2017-12-21 17:48:59 +0530

    improves cp.c 	1. uses unsigned long int for file size 	2. asserts source path != destination path 	3. prints error messages to stderr instead of stdout

--- cp.c
+++ cp.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
 
 Basic copy
+Validates source path and destination path
+	1. Asserts that they are different
 Overwrites destination file if already exists
 
 */
@@ -12,32 +15,37 @@ int main(int argc, char *argv[])
 {
 	// argument validation
 	if (argc != 3) {
-		printf("Usage: %s <source-file-path> <destination-file-path>\n", argv[0]);
+		fprintf(stderr, "Usage: %s <source-file-path> <destination-file-path>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-
 	char *sourcePath = argv[1];
 	char *destinationPath = argv[2];
+	// source path and destination path cannot be equal
+	if (strcmp(sourcePath, destinationPath) == 0) {
+		fprintf(stderr, "source path and destination path cannot be same\n");
+		exit(EXIT_FAILURE);		
+	}
+	
 	FILE *source, *destination;
 
 	source = fopen(sourcePath, "rb");
 	if (source == NULL) {
-		printf("Cannot open source file : %s\n", sourcePath);
+		fprintf(stderr, "Cannot open source file : %s\n", sourcePath);
 		exit(EXIT_FAILURE);
 	}
 	// get file size
 	fseek(source, 0, SEEK_END);
-	int fileSize = ftell(source);
+	unsigned long int fileSize = ftell(source);
 	fseek(source, 0, SEEK_SET);
 	
-	destination = fopen(destinationPath, "w");
+	destination = fopen(destinationPath, "wb");
 	if (destination == NULL) {
 		fclose(source);
-		printf("Cannot open destination file : %s\n", destinationPath);
+		fprintf(stderr, "Cannot open destination file : %s\n", destinationPath);
 		exit(EXIT_FAILURE);
 	}
 
-	for (int i = 0; i < fileSize; ++i) {
+	for (unsigned long int i = 0; i < fileSize; ++i) {
 		fputc(fgetc(source), destination);
 	}
 
