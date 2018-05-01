9b82bb64b0deeb07b26df58a0678adcbf2eb01ac
Yashasvi-Sriram <yash.3997@gmail.com>
2018-05-01 20:17:27 +0530

    removes unnecessary @ensure_csrf decoration on goal_glance page

--- django-server/webinterface/views/template.py
+++ django-server/webinterface/views/template.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import ensure_csrf_cookie
 
 
 def index(request):
     return render(request, 'webinterface/index/index.html')
 
 
-@ensure_csrf_cookie
 def goal_glance(request):
     return render(request, 'webinterface/goal/glance.html')
 
