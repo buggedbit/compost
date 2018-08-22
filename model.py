e575094677df47efcf05b5d597a510618f30a8c6
Yashasvi-Sriram <yash.3997@gmail.com>
2018-08-22 17:05:52 +0530

    adds log messages

--- model.py
+++ model.py
@@ -1,13 +1,15 @@
 from keras.layers import Embedding, Flatten, Dense
-import numpy as np
 from keras.models import Sequential
+
 from preprocessing import preprocess_essay_data, load_word_embeddings, get_word_embeddings_matrix
 
+# parameters
 max_data_length = 2000
+
+# pre processing
+print('-------- -------- Pre Processing -------- --------')
 data, labels, vocabulary_size, word_index = preprocess_essay_data('data/all.tsv', max_length=max_data_length)
-
 word_embeddings = load_word_embeddings('word_embeddings/glove.100K.300d.txt')
-
 embeddings_matrix = get_word_embeddings_matrix(word_embeddings, vocabulary_size, word_index)
 
 # define model
@@ -16,11 +18,13 @@ model.add(Embedding(vocabulary_size, 300, weights=[embeddings_matrix], input_len
 model.add(Flatten())
 model.add(Dense(1, activation='sigmoid'))
 
+print('-------- -------- Compiling Model -------- --------')
 # compile the model
 model.compile(optimizer='adagrad', loss='mse', metrics=['mse'])
 # summarize the model
 print(model.summary())
 
+print('-------- -------- Fitting Model -------- --------')
 # fit the model
 model.fit(data, labels, epochs=50, verbose=0)
 # evaluate the model
