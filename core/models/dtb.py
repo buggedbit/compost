cee5bf178f06d47d9713c02cec3bdd3117e920d0
Yashasvi-Sriram <yash.3997@gmail.com>
2018-09-13 01:40:49 +0530

    moves TimeStub class to dtb.py and removes timestub.py

--- core/models/dtb.py
+++ core/models/dtb.py
@@ -4,11 +4,35 @@ import math
 from django.db import models
 from datetime import datetime as dt, timedelta as td
 from core.models.dtt import DTT
-from core.timestub import TimeStub
 
 
 def to_microseconds(time_delta):
     return time_delta.days * 86400000000 + time_delta.seconds * 1000000 + time_delta.microseconds
+
+
+class TimeStub:
+    def __init__(self, epoch=dt.now(), end=dt.now(), duration=td()):
+        self.epoch = epoch
+        self.end = end
+        self.duration = duration
+
+    def is_valid(self):
+        if self.epoch is not None and isinstance(self.epoch, dt) \
+                and self.end is not None and isinstance(self.end, dt) \
+                and self.duration is not None and isinstance(self.duration, td) and self.duration > td() \
+                and self.epoch + self.duration == self.end:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return str((self.epoch, self.end))
+
+    def __hash__(self):
+        return hash((self.epoch, self.end, self.duration))
+
+    def __eq__(self, other):
+        return (self.epoch, self.end, self.duration) == (other.epoch, other.end, other.duration)
 
 
 class DTB(models.Model):
