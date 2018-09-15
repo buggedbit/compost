7a1d170e5888eafc2d418f7a67c340c10176957d
Yashasvi-Sriram <yash.3997@gmail.com>
2018-09-15 20:53:38 +0530

    implements next and previous (months) feature in GoalSnapshotMonth 	1. uses two variables currMonth and currYear 	2. all momemt() instances are set year month and date explicitly 	3. shows year and month at bottom right

--- interface/static/interface/escapeerrandsjs/goal/snapshot_viewer/goal-snapshot-month.jsx
+++ interface/static/interface/escapeerrandsjs/goal/snapshot_viewer/goal-snapshot-month.jsx
@@ -42,7 +42,8 @@ let GoalSnapshotDay = React.createClass({
                 <span style={{color: e.color}}>{e.description}</span>
             </div>
         });
-        let bgColor = this.getColorOfDay(pr.day === moment().date());
+        let _now = moment();
+        let bgColor = this.getColorOfDay(pr.day === _now.date() && pr.month - 1 === _now.month() && pr.year === _now.year());
         return (
             <div style={{
                 position: 'absolute',
@@ -54,7 +55,7 @@ let GoalSnapshotDay = React.createClass({
                 overflowY: 'auto',
                 backgroundColor: bgColor,
             }}>
-                <div><b>{moment().date(pr.day).format('Do ddd')}</b></div>
+                <div><b>{moment().date(pr.day).month(pr.month - 1).year(pr.year).format('Do ddd')}</b></div>
                 <div className="collection">
                     {goalNames}
                 </div>
@@ -64,91 +65,21 @@ let GoalSnapshotDay = React.createClass({
 });
 
 let GoalSnapshotMonth = React.createClass({
+    currMonth: moment().month() + 1,
+    currYear: moment().year(),
     getInitialState: function () {
-        let year = moment().year();
-        let month = moment().month() + 1;
-        return {perDayGoals: {}, rowMajorRendering: true, month: month, year: year};
+        return {perDayGoals: {}, rowMajorRendering: true,};
     },
     switchLayoutRendering: function () {
         this.setState((prevState, props) => {
             return {rowMajorRendering: !prevState.rowMajorRendering};
         });
     },
-    getGoalSnapshotDay: function (day, goals) {
-        let refs = this.refs;
-        let st = this.state;
-        let W = $(refs.snapshotMonth).width();
-        let H = $(refs.snapshotMonth).height();
-        if (st.rowMajorRendering) {
-            // row major layout
-            let w = W / 7;
-            let h = H / 5;
-            let l = ((day - 1) % 7) * w;
-            let t = parseInt((day - 1) / 7) * h;
-            return <GoalSnapshotDay key={[day, '-'] + goals.map((e) => e.id)}
-                                    day={day}
-                                    widthPx={w}
-                                    heightPx={h}
-                                    leftPx={l}
-                                    topPx={t}
-                                    goals={goals}/>
-        } else {
-            // column major layout
-            let w = W / 5;
-            let h = H / 7;
-            let t = ((day - 1) % 7) * h;
-            let l = parseInt((day - 1) / 7) * w;
-            return <GoalSnapshotDay key={[day, '-'] + goals.map((e) => e.id)}
-                                    day={day}
-                                    widthPx={w}
-                                    heightPx={h}
-                                    leftPx={l}
-                                    topPx={t}
-                                    goals={goals}/>
-        }
-    },
-    render: function () {
-        // aliases
-        let st = this.state;
-        // day wise snapshots
-        let daySnapshots = [];
-        let numDaysInMonth = moment().daysInMonth();
-        for (let day = 1; day <= numDaysInMonth; ++day) {
-            daySnapshots.push(this.getGoalSnapshotDay(day, st.perDayGoals[day] === undefined ? [] : st.perDayGoals[day]));
-        }
-        return (
-            <div style={{
-                position: 'relative',
-                width: '100%',
-                height: '100%',
-            }} ref="snapshotMonth">
-                {daySnapshots}
-                <div style={{
-                    position: 'absolute',
-                    right: '20px',
-                    bottom: '100px',
-                }}>
-                    <button className="waves-effect waves-light btn-large">{moment().format('MMMM')}</button>
-                </div>
-                <div style={{
-                    position: 'absolute',
-                    right: '20px',
-                    bottom: '20px',
-                }}>
-                    <button className="btn btn-floating btn-large"><i
-                        className="material-icons"
-                        title="Switch layout"
-                        onClick={this.switchLayoutRendering}>{st.rowMajorRendering ? "view_week" : "view_stream"}</i>
-                    </button>
-                </div>
-            </div>
-        );
-    },
-    componentDidMount: function () {
+    fetchSnapshotOfMonth: function (year, month) {
         let self = this;
         $.post(this.props.snapShotMonthUrl, {
-            year: self.state.year,
-            month: self.state.month,
+            year: year,
+            month: month,
         }).done((r) => {
             let json = JSON.parse(r);
             if (json.status === -1) {
@@ -167,6 +98,94 @@ let GoalSnapshotMonth = React.createClass({
                 return {perDayGoals: {}};
             });
         });
+    },
+    shiftCurrentSnapshot: function (numMonths) {
+        let prev = moment().date(1).month(this.currMonth - 1).year(this.currYear);
+        let now = prev.add(numMonths, 'month');
+        this.currMonth = now.month() + 1;
+        this.currYear = now.year();
+        this.fetchSnapshotOfMonth(this.currYear, this.currMonth);
+    },
+    getGoalSnapshotDay: function (day, goals) {
+        let refs = this.refs;
+        let st = this.state;
+        let W = $(refs.snapshotMonth).width();
+        let H = $(refs.snapshotMonth).height();
+        if (st.rowMajorRendering) {
+            // row major layout
+            let w = W / 7;
+            let h = H / 5;
+            let l = ((day - 1) % 7) * w;
+            let t = parseInt((day - 1) / 7) * h;
+            return <GoalSnapshotDay key={[day, '-'] + goals.map((e) => e.id)}
+                                    day={day}
+                                    month={this.currMonth}
+                                    year={this.currYear}
+                                    widthPx={w}
+                                    heightPx={h}
+                                    leftPx={l}
+                                    topPx={t}
+                                    goals={goals}/>
+        } else {
+            // column major layout
+            let w = W / 5;
+            let h = H / 7;
+            let t = ((day - 1) % 7) * h;
+            let l = parseInt((day - 1) / 7) * w;
+            return <GoalSnapshotDay key={[day, '-'] + goals.map((e) => e.id)}
+                                    day={day}
+                                    month={this.currMonth}
+                                    year={this.currYear}
+                                    widthPx={w}
+                                    heightPx={h}
+                                    leftPx={l}
+                                    topPx={t}
+                                    goals={goals}/>
+        }
+    },
+    render: function () {
+        // aliases
+        let st = this.state;
+        // day wise snapshots
+        let daySnapshots = [];
+        let numDaysInMonth = moment().year(this.currYear).month(this.currMonth - 1).date(1).daysInMonth();
+        for (let day = 1; day <= numDaysInMonth; ++day) {
+            daySnapshots.push(this.getGoalSnapshotDay(day, st.perDayGoals[day] === undefined ? [] : st.perDayGoals[day]));
+        }
+        return (
+            <div style={{
+                position: 'relative',
+                width: '100%',
+                height: '100%',
+            }} ref="snapshotMonth">
+                {daySnapshots}
+                <div style={{
+                    position: 'absolute',
+                    right: '20px',
+                    bottom: '100px',
+                }}>
+                    <button onClick={() => this.shiftCurrentSnapshot(-1)}>prev</button>
+                    <button className="waves-effect waves-light btn-large">
+                        {moment().year(this.currYear).month(this.currMonth - 1).date(1).format('MMM YYYY')}
+                    </button>
+                    <button onClick={() => this.shiftCurrentSnapshot(1)}>next</button>
+                </div>
+                <div style={{
+                    position: 'absolute',
+                    right: '20px',
+                    bottom: '20px',
+                }}>
+                    <button className="btn btn-floating btn-large"><i
+                        className="material-icons"
+                        title="Switch layout"
+                        onClick={this.switchLayoutRendering}>{st.rowMajorRendering ? "view_week" : "view_stream"}</i>
+                    </button>
+                </div>
+            </div>
+        );
+    },
+    componentDidMount: function () {
+        this.fetchSnapshotOfMonth(this.currYear, this.currMonth);
     }
 });
 
