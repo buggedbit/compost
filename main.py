2bb185aaf1a1370f9a4c2b324aeb58f9f5b11f82
Yashasvi Sriram <yash.3997@gmail.com>
2018-11-04 21:31:39 +0530

    uses argparse lib, improves log

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+import argparse
 import sys
 import os
 from model_generator import generate_model
-from preprocessor import generate_tokenizer_on_all_essays, encode_essay_data, load_word_embeddings_dict, \
-    get_word_embeddings_matrix
+from preprocessor import generate_tokenizer_on_all_essays, encode_essay_data, load_word_embeddings_dict, get_word_embeddings_matrix
 from quadratic_weighted_kappa import quadratic_weighted_kappa
 import numpy as np
 import matplotlib
-matplotlib.use("pdf")
+matplotlib.use('pdf')
 from matplotlib import pyplot as plt
 
 def get_qwk(model, essays, true_scores, min_score, max_score):
@@ -16,38 +16,49 @@ def get_qwk(model, essays, true_scores, min_score, max_score):
     qwk = quadratic_weighted_kappa(predicted_t_scores, true_scores, min_rating=min_score, max_rating=max_score)
     return qwk
 
-# hyper parameters
-assert(len(sys.argv) == 8)
-NUM_EPOCHS = int(sys.argv[1])
-MIN_SCORE = int(sys.argv[2])
-MAX_SCORE = int(sys.argv[3])
-TRAINING_DATA_FILE = sys.argv[4]
-VALIDATION_DATA_FILE = sys.argv[5]
-WORD_EMB_FILE = sys.argv[6]
-OUTPUT_DIR = sys.argv[7]
+# arguments
+parser = argparse.ArgumentParser()
+# required args
+parser.add_argument('--NUM_EPOCHS', required=True, type=int)
+parser.add_argument('--MIN_SCORE', required=True, type=int)
+parser.add_argument('--MAX_SCORE', required=True, type=int)
+parser.add_argument('--TRAINING_DATA_FILE', required=True)
+parser.add_argument('--VALIDATION_DATA_FILE', required=True)
+parser.add_argument('--WORD_EMB_FILE', required=True)
+parser.add_argument('--OUTPUT_DIR', required=True)
+# optional args
+parser.add_argument('--MAX_ESSAY_LENGTH', type=int, default=2000)
+parser.add_argument('--EMBEDDING_SIZE', type=int, default=300)
+parser.add_argument('--LOG_FILE', default='log.txt')
+parser.add_argument('--VOCAB_FILE', default='data/vocab_db.txt')
+parser.add_argument('--MODEL_DEF_FILE', default='model.json')
+parser.add_argument('--STATS_GRAPH_FILE', default='training_stats.png')
+args = parser.parse_args()
 
-assert(os.path.isdir(OUTPUT_DIR))
-MAX_ESSAY_LENGTH = 2000
-EMBEDDING_SIZE = 300
+# assert output dir exists
+assert(os.path.isdir(args.OUTPUT_DIR))
 
-# log file
-sys.stdout = open('%s/log.txt' % OUTPUT_DIR, 'w')
+# open log file
+sys.stdout = open('%s/%s' % (args.OUTPUT_DIR, args.LOG_FILE), 'w')
+
+# log arguments
+print(args)
 
 # pre processing
 print('-------- -------- Pre Processing')
-tokenizer = generate_tokenizer_on_all_essays(('data/vocab_db.txt',))
+tokenizer = generate_tokenizer_on_all_essays((args.VOCAB_FILE,))
 vocab_size = len(tokenizer.word_index) + 1
-tr_essays, tr_n_scores, tr_t_scores = encode_essay_data(TRAINING_DATA_FILE, MAX_ESSAY_LENGTH, tokenizer)
-va_essays, va_n_scores, va_t_scores = encode_essay_data(VALIDATION_DATA_FILE, MAX_ESSAY_LENGTH, tokenizer)
-word_embeddings_dict = load_word_embeddings_dict(WORD_EMB_FILE)
-embeddings_matrix = get_word_embeddings_matrix(word_embeddings_dict, tokenizer.word_index, EMBEDDING_SIZE)
+tr_essays, tr_n_scores, tr_t_scores = encode_essay_data(args.TRAINING_DATA_FILE, args.MAX_ESSAY_LENGTH, tokenizer)
+va_essays, va_n_scores, va_t_scores = encode_essay_data(args.VALIDATION_DATA_FILE, args.MAX_ESSAY_LENGTH, tokenizer)
+word_embeddings_dict = load_word_embeddings_dict(args.WORD_EMB_FILE)
+embeddings_matrix = get_word_embeddings_matrix(word_embeddings_dict, tokenizer.word_index, args.EMBEDDING_SIZE)
 
 print('-------- -------- Model generation')
-model = generate_model(vocab_size, MAX_ESSAY_LENGTH, embeddings_matrix)
+model = generate_model(vocab_size, args.MAX_ESSAY_LENGTH, embeddings_matrix)
 
 # save the model
 model_json = model.to_json()
-with open('%s/model.json' % OUTPUT_DIR, 'w') as json_file:
+with open('%s/%s' % (args.OUTPUT_DIR, args.MODEL_DEF_FILE), 'w') as json_file:
     json_file.write(model_json)
 
 # write to stdout
@@ -57,7 +68,7 @@ tr_losses = []
 va_losses = []
 tr_qwks = []
 va_qwks = []
-for epoch in range(NUM_EPOCHS):
+for epoch in range(args.NUM_EPOCHS):
     print('-------- -------- Epoch = %d' % epoch)
 
     print('         -------- Fitting Model')
@@ -73,14 +84,16 @@ for epoch in range(NUM_EPOCHS):
     print('va_loss =', loss)
 
     print('         -------- Validating Model')
-    qwk = get_qwk(model, va_essays, va_t_scores, MIN_SCORE, MAX_SCORE)
     # Calculate TR QWK
-    qwk = get_qwk(model, tr_essays, tr_t_scores, MIN_SCORE, MAX_SCORE)
+    qwk = get_qwk(model, tr_essays, tr_t_scores, args.MIN_SCORE, args.MAX_SCORE)
     tr_qwks.append(qwk)
     print('tr_qwk =', qwk)
+    
     # Calculate VA QWK
+    qwk = get_qwk(model, va_essays, va_t_scores, args.MIN_SCORE, args.MAX_SCORE)
     va_qwks.append(qwk)
     print('va_qwk =', qwk)
+    
     # max VA QWK until now
     print('max va_qwk until now = %f @ %d epoch' % (va_qwks[np.argmax(va_qwks)], np.argmax(va_qwks)))
 
@@ -94,7 +107,7 @@ for epoch in range(NUM_EPOCHS):
     print('         -------- Saving Model')
     # save model if it has best validation accuracy or training accuracy until now
     if epoch == np.argmax(va_qwks) or epoch == np.argmax(tr_qwks):
-        model.save_weights('%s/model%d.h5' % (OUTPUT_DIR, epoch))
+        model.save_weights('%s/model%d.h5' % (args.OUTPUT_DIR, epoch))
 
     # write to stdout
     sys.stdout.flush()
@@ -106,15 +119,16 @@ print('validation_losses = ', va_losses)
 print('max_tr_qwk = %f @ %d epoch' % (tr_qwks[np.argmax(tr_qwks)], np.argmax(tr_qwks)))
 print('max_va_qwk = %f @ %d epoch' % (va_qwks[np.argmax(va_qwks)], np.argmax(va_qwks)))
 
-epochs = [i for i in range(NUM_EPOCHS)]
-baseline = [0 for i in range(NUM_EPOCHS)]
+# plot all metrics in a graph
+epochs = [i for i in range(args.NUM_EPOCHS)]
+baseline = [0 for i in range(args.NUM_EPOCHS)]
 plt.title('max va qwk = %f @ %d epoch\n max tr qwk = %f @ %d epoch' % (va_qwks[np.argmax(va_qwks)], np.argmax(va_qwks), tr_qwks[np.argmax(tr_qwks)], np.argmax(tr_qwks)))
-plt.xlabel("epochs")
-plt.ylabel("value")
+plt.xlabel('epochs')
+plt.ylabel('value')
 plt.plot(epochs, baseline, '-', color='black')
 plt.plot(epochs, tr_qwks)
 plt.plot(epochs, va_qwks)
 plt.plot(epochs, tr_losses)
 plt.plot(epochs, va_losses)
 plt.gca().legend(('y=0','training QWK', 'validation QWK', 'training loss', 'validation loss'))
-plt.savefig('%s/trainingStats.png' % OUTPUT_DIR)
+plt.savefig('%s/%s' % (args.OUTPUT_DIR, args.STATS_GRAPH_FILE))
